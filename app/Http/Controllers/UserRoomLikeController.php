d0af096ae23d3d5b0c1507b03a6261617b89aeac
fajarikas <118388472+fajarikas@users.noreply.github.com>
2025-12-08 10:30:15 +0700

    feat: user room like

---
+++ app/Http/Controllers/UserRoomLikeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserRoomLike;
+use Illuminate\Http\Request;
+
+class UserRoomLikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserRoomLike $userRoomLike)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserRoomLike $userRoomLike)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserRoomLike $userRoomLike)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserRoomLike $userRoomLike)
+    {
+        //
+    }
+}
