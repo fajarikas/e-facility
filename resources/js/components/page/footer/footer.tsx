794d7809f0d6c93b71cc28cc8e8092609c538365
fajarikas <118388472+fajarikas@users.noreply.github.com>
2026-01-15 11:11:53 +0700

    feat: footer

--- resources/js/components/page/footer/footer.tsx
+++ resources/js/components/page/footer/footer.tsx
@@ -1,17 +1,97 @@
-import React from 'react'
-
-type Props = {}
+import { BiLogoGmail } from 'react-icons/bi';
+import { FaPhone } from 'react-icons/fa6';
+import { SiOrganicmaps } from 'react-icons/si';
+type Props = {};
 
 export const Footer = (props: Props) => {
-  return (
-    <div>
-        <div className="bg-[#018fd3] w-full px-4 py-5 lg:px-0 lg:py-14 mb-[50px] mr-[58px] rounded-3xl">
-            <div className="mx-auto w-full max-w-[1280px] text-center">
-                <h2 className="text-white font-semibold">© 2024 E-Facility. All rights reserved.</h2>
+    return (
+        <div className="mx-auto my-10 w-11/12 max-w-7xl">
+            <div className="bg-footer w-full rounded-3xl px-4 py-5 text-white lg:px-0 lg:py-10">
+                <div className="px-10">
+                    <div className="flex items-start justify-between">
+                        <div className="flex w-4/6 flex-col">
+                            <h3 className="text-3xl font-extrabold">
+                                Hubungi kami
+                            </h3>
+                            <div className="mt-3">
+                                <div className="flex items-center space-x-3">
+                                    <FaPhone size={13} color="white" />
+                                    <p className="font-medium">
+                                        +628 1171176160
+                                    </p>
+                                </div>
+                                <div className="flex items-center space-x-3">
+                                    <BiLogoGmail size={13} color="white" />
+                                    <p className="font-medium">
+                                        bpmp.babel@kemdikbud.go.id
+                                    </p>
+                                </div>
+                                <div className="flex items-center space-x-3">
+                                    <SiOrganicmaps size={13} color="white" />
+                                    <p className="font-medium">
+                                        Jl. Pulau Bangka, Padang Baru, Kec.
+                                        Bukitintan, Kabupaten Bangka Tengah,
+                                        Kepulauan Bangka Belitung 33684
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex w-2/6 flex-col">
+                            <h3 className="text-right text-3xl font-extrabold">
+                                Unit Layanan Terpadu
+                            </h3>
+                            <div className="mt-3">
+                                <p className="text-right font-medium text-white">
+                                    Jam Layanan
+                                </p>
+                                <p className="text-right font-medium text-white">
+                                    08.00 - 16.00 WIB
+                                </p>
+                                <div className="mt-4">
+                                    <p className="text-right font-bold text-white">
+                                        Follow Sosial Media Kami
+                                    </p>
+                                    <div className="mt-1 flex items-center justify-end space-x-1">
+                                        <img
+                                            className="w-7"
+                                            src="/images/icons/fb.png"
+                                        />
+                                        <img
+                                            className="w-7"
+                                            src="/images/icons/ig.png"
+                                        />
+                                        <img
+                                            className="w-7"
+                                            src="/images/icons/x.png"
+                                        />
+                                        <img
+                                            className="w-7"
+                                            src="/images/icons/yt.png"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-10 border-t-2 border-t-white">
+                    <div className="flex w-full items-end justify-between px-10">
+                        <div className="">
+                            <p className="mt-4 text-3xl font-extrabold text-white">
+                                BPMP BABEL
+                            </p>
+                            <p className="mt-1">
+                                © 2025 BPMP Provinsi Kepulauan Bangka Belitung
+                            </p>
+                        </div>
+                        <p className="mt-1 font-semibold">
+                            Pangkal Pinang, Kepulauan Bangka Belitung
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Footer
+export default Footer;
--- resources/js/components/page/main/calltoaction.tsx
+++ resources/js/components/page/main/calltoaction.tsx
@@ -1,48 +1,44 @@
-import { Button } from "@/components/button/button";
-import { ButtonHub } from "@/components/button/button";
-import React from "react";
+import { Button, ButtonHub } from '@/components/button/button';
 
 type Props = {};
 
 const CallToAction = (props: Props) => {
-  return (
-    <div className="w-full bg-calltoaction px-4 py-8 lg:py-14">
-      <div className="mx-auto max-w-11/12 flex flex-col lg:flex-row items-center justify-between gap-10">
+    return (
+        <div className="bg-calltoaction w-full px-4 py-8 lg:py-14">
+            <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-10 lg:flex-row">
+                <div className="flex max-w-[500px] flex-col space-y-3 lg:space-y-4">
+                    <h1 className="text-2xl font-bold text-black lg:text-4xl">
+                        Siap Menyewa Fasilitas Instansi Kami?
+                    </h1>
 
-        <div className="flex flex-col space-y-3 lg:space-y-4 max-w-[500px]">
-          <h1 className="text-2xl font-bold text-black lg:text-4xl">
-            Siap Menyewa Fasilitas Instansi Kami?
-          </h1>
+                    <p className="text-sm font-normal text-black lg:text-base">
+                        Mulai cari dan pesan fasilitas BMN Anda hari ini. Proses
+                        transparan, legal, dan tanpa ribet birokrasi.
+                    </p>
 
-          <p className="text-sm text-black font-normal lg:text-base">
-            Mulai cari dan pesan fasilitas BMN Anda hari ini. Proses transparan,
-            legal, dan tanpa ribet birokrasi.
-          </p>
+                    <div className="mt-2 flex flex-col gap-2">
+                        <Button
+                            name="Cek Ketersediaan & Pesan Sekarang"
+                            onClick={() => console.log('Pesan clicked')}
+                        />
+                        <ButtonHub
+                            name="Hubungi Kami (Untuk Info Lebih Lanjut)"
+                            onClick={() => console.log('Hubungi clicked')}
+                            image={['/images/icons/whatsapp.svg']}
+                        />
+                    </div>
+                </div>
 
-          <div className="flex flex-col gap-2 mt-2">
-            <Button
-              name="Cek Ketersediaan & Pesan Sekarang"
-              onClick={() => console.log("Pesan clicked")}
-            />
-            <ButtonHub
-              name="Hubungi Kami (Untuk Info Lebih Lanjut)"
-              onClick={() => console.log("Hubungi clicked")}
-              image={['/images/icons/whatsapp.svg']}
-            />
-          </div>
+                <div className="calltoaction-img hidden w-full lg:block lg:w-[420px]">
+                    <img
+                        src="/images/landingpage/calltoact.webp"
+                        alt="Call to Action Image"
+                        className="h-auto w-full object-contain"
+                    />
+                </div>
+            </div>
         </div>
-
-        <div className="hidden lg:block w-full lg:w-[420px] calltoaction-img">
-          <img
-            src="/images/landingpage/calltoact.webp" 
-            alt="Call to Action Image"
-            className="w-full h-auto object-contain"
-          />
-        </div>
-
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CallToAction;
