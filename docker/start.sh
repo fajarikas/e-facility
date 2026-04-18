24f6ad93f734f0d80e4763edc5640154b1ff5336
fajarikas <fajarika.2021@student.uny.ac.id>
2026-04-19 03:42:44 +0700

    feat: add docker

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Optimasi Laravel untuk Production
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Jalankan migrasi database otomatis (Opsional tapi disarankan untuk awal)
+# php artisan migrate --force
+
 # Jalankan PHP-FPM di background
 php-fpm -D
 
